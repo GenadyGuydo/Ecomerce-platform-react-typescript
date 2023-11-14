0f3bcfb7b91d41e12375eab4a276bde5fe40fefe
GenadyGuydo <law.guydo@bk.ru>
2023-11-14 10:19:31 +0300

    creating react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,14 @@
 import Header from "./components/header";
-import { sampleProducts } from "./data";
-import { Row, Container, Col} from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div className="d-flex flex-column vh-100">
       <Header />
 
       <main>
-        <Container className="mt-3" >
-          <Row>
-            {sampleProducts.map((product) => (
-              <Col key={product.slug} sm={6} md={4} lg={3} className="col">
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="prod-image"
-                />
-                <h2>{product.name}</h2>
-                <p>${product.price}</p>
-              </Col>
-            ))}
-          </Row>
+        <Container className="mt-3">
+          <Outlet />
         </Container>
       </main>
       <footer> all right reserved</footer>
