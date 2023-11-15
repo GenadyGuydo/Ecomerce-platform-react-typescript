13393381bf8b80a8f96011b68915b35e68357e70
GenadyGuydo <law.guydo@bk.ru>
2023-11-15 11:37:53 +0300

    complete fetch commit

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,29 +3,52 @@ import { sampleProducts } from "../data";
 import { Link } from "react-router-dom";
 import { State } from "../types/State";
 import { Action } from "../types/Action";
-
-
+import { useReducer, useEffect } from "react";
+import { ApiError } from "../types/ApiError";
+import axios from "axios";
+import { getError } from "../utils";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 const initialState: State = {
   products: [],
   loading: true,
-  error: '',
-}
+  error: "",
+};
 
-const reducer =(state: State , action: Action) =>{
-   switch (action.type) {
-    case 'FETCH_REQUEST':
-      return {...state, loading: true}
-    case 'FETCH_SUCCESS':
-        return {...state, product:action.payload, loading: false}
-    case 'FETCH_FAIL':
-          return {...state, product:action.payload, loading: false}   
-    }
-}
-
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, product: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, product: action.payload, loading: false };
+  }
+};
 
 const HomePage = () => {
-  return (
+  const [{ loading, error, products }, dispatch] = useReducer<
+    React.Reducer<State, Action>
+  >(reducer, initialState);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(err as ApiError) });
+      }
+    };
+    fetchData();
+  }, []);
+  return loading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant="danger">{error}</MessageBox>
+  ) : (
     <div>
       <Row>
         {sampleProducts.map((product) => (
